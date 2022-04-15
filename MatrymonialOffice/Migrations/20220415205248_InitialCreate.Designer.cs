23952ffd28b2232938560b5a735e66878a190894
Szymon Koński <01151615@pw.edu.pl>
2022-04-15 22:53:47 +0200

    Fixed latitude and longitude range

--- MatrymonialOffice/Migrations/20220415205248_InitialCreate.Designer.cs
+++ MatrymonialOffice/Migrations/20220415205248_InitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using MatrymonialOffice;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MatrymonialOffice.Migrations
 {
     [DbContext(typeof(MatrymonialOfficeDbContext))]
-    partial class MatrymonialOfficeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220415205248_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
