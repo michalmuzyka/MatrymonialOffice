175affd561d94f5788f7ed006d4da7f34af95418
Wojciech <wojtek2288@gmail.com>
2022-05-01 16:44:23 +0200

    Add user UI

---
+++ MatrymonialOffice/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrymonialOffice
+{
+    public static class Extensions
+    {
+        public static void SafeClearItems(this ComboBox comboBox)
+        {
+            foreach (var item in new ArrayList(comboBox.Items))
+            {
+                comboBox.Items.Remove(item);
+            }
+        }
+    }
+}
---
+++ MatrymonialOffice/UserMatchesForm.cs
@@ -0,0 +1,63 @@
+﻿using MatrymonialOffice.Alghoritms;
+using MatrymonialOffice.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrymonialOffice
+{
+    public class UserWithRatio : User
+    {
+        public double MatchRatio { get; set; }
+
+        public UserWithRatio(User user, double matchRatio)
+        {
+            foreach (var prop in user.GetType().GetProperties())
+            {
+                this.GetType().GetProperty(prop.Name)!.SetValue(this, prop.GetValue(user, null), null);
+            }
+            MatchRatio = matchRatio;
+        }
+    }
+
+    public partial class UserMatchesForm : Form
+    {
+        MatrymonialOfficeDbContext dbContext;
+        List<User> users;
+        public PartnerRequirements requirements { get; set; }
+
+        public UserMatchesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserMatchesForm_Load(object sender, EventArgs e)
+        {
+            dbContext = new MatrymonialOfficeDbContext();
+            users = dbContext.Users.ToList<User>();
+            this.Icon = Properties.Resources.heart_icon;
+            tableLayoutPanel1.BackColor = ColorTranslator.FromHtml("#FAE3D9");
+            dataGridView1.DataSource = CalculateMatchRatio(users);
+            dataGridView1.Columns.RemoveAt(1);
+        }
+
+        private List<UserWithRatio> CalculateMatchRatio(List<User> users)
+        {
+            var matchedUsers = new List<UserWithRatio>();
+
+            foreach (var user in users)
+            {
+                var ratio = Match.MatchingFactor(user, requirements);
+                matchedUsers.Add(new UserWithRatio(user, ratio));
+            }
+
+            return matchedUsers.OrderByDescending(u => u.MatchRatio).Take(10).ToList();
+        }
+    }
+}
